3d59ff979395ba597e85159b046bf534d4daa030
Alex Sanders <alexsanders@Alexs-MacBook-Pro.local>
2013-04-17 23:43:15 +0100

    add some more images

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
 // This array contains the URLs that will appear on dantombs.net.
 // Add/remove as you see fit.
-URLs = [
-  "http://www.160grams.com/exclusives/vision-and-sound-dan-tombs", 
-  "http://www.inverted-audio.com/dan-tombs/", 
-  "http://www.futuresequence.com/article/discover-dan-tombs/", 
-  "http://www.youtube.com/watch?v=uX3n_eWN1Vc", 
-  "http://norwichmusic.com/post/28900046565/awesome-dan-tombs-live-visuals-for-factory", 
-  "https://www.rsn-tickets.com/event/inverted-audio-electronic-explorations-present-notown-luke-abbott-dam-mantle-wesley-matsell-dan-tombs/891/", 
+window.URLs = [
+  "http://www.160grams.com/exclusives/vision-and-sound-dan-tombs",
+  "http://www.inverted-audio.com/dan-tombs/",
+  "http://www.futuresequence.com/article/discover-dan-tombs/",
+  "http://www.youtube.com/watch?v=uX3n_eWN1Vc",
+  "http://norwichmusic.com/post/28900046565/awesome-dan-tombs-live-visuals-for-factory",
+  "https://www.rsn-tickets.com/event/inverted-audio-electronic-explorations-present-notown-luke-abbott-dam-mantle-wesley-matsell-dan-tombs/891/",
   "http://www.nme.com/nme-video/youtube/id/IkDrteJqSLk",
   "http://seamsmusic.com/post/46206147720/a-taste-of-dan-tombs-doing-visuals-at",
   "http://thequietus.com/articles/11443-inverted-audio-luke-abbott-dan-tombs-mister-sushi",
@@ -16,7 +16,9 @@ URLs = [
 // The site will randomly try and show one of these as the background image. 
 // The image needs to be present in the 'images' folder.
 // You could always add strings that won't find images to show some white background for a while if you like.
-images = [
+window.images = [
   "brutality.gif",
-  "muscle_beach.gif"
-] 
+  "muscle_beach.gif",
+  "hyetal.gif",
+  "ica.gif"
+];
