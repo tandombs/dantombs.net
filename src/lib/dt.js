edfe6fa5874fb55560593ecd44db0f484dcf7ab2
Alex Sanders <alexsanders@Alexs-MacBook-Pro.local>
2013-04-18 00:04:54 +0100

    add self-shout out

--- src/lib/dt.js
+++ src/lib/dt.js
@@ -1,3 +1,4 @@
 // @codekit-append "../vendor/jquery-1.9.1.js"
 // @codekit-append "../vendor/jquery.jrumble.1.3.js"
 // @codekit-append "./scripts.js"
+if(console){console.log("Programmed by @asanders. Source available at https://github.com/tandombs/dantombs.net.");}
