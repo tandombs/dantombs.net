4321fa390ea0f17ec771a570ff3dd34fe4edfd86
Dan Tombs <dan_tombs@me.com>
2013-04-22 19:01:03 +0100

    update links and images

--- js/data.js
+++ js/data.js
@@ -10,6 +10,21 @@ window.URLs = [
   "http://www.nme.com/nme-video/youtube/id/IkDrteJqSLk",
   "http://seamsmusic.com/post/46206147720/a-taste-of-dan-tombs-doing-visuals-at",
   "http://thequietus.com/articles/11443-inverted-audio-luke-abbott-dan-tombs-mister-sushi",
+  "http://www.mirabcn.cat/artists/dan-tombs-uk/?lang=en",
+  "http://www.broadway.org.uk/pages/91",
+  "http://www.160grams.com/exclusives/vision-and-sound-dan-tombs",
+  "http://levfestival.com/13/en/program/jon-hopkins-dan-tombs/",
+  "http://www.residentadvisor.net/forum-read.aspx?id=205913",
+  "http://www.youtube.com/watch?v=OGBvuxDNVPM",
+  "http://vimeo.com/37237788"
+  "http://www.youtube.com/watch?v=S0aUveZfyEY",
+  "http://tate-collectives.tumblr.com/post/30169418792/factory-floor-this-evening-as-part-of"
+  "http://www.tumblr.com/tagged/dan+tombs",
+  "https://whatson.bfi.org.uk/Online/default.asp?doWork::WScontent::loadArticle=Load&BOparam::WScontent::loadArticle::article_id=44783CE0-8E06-4B63-BAF2-5523A72B98BA&sessionlanguage=&SessionSecurity::linkName=",
+  "http://www.ica.org.uk/?lid=32140",
+  "http://www.laboralcentrodearte.org/es/recursos/personas/jon-hopkins-dan-tombs",
+  "http://noisey.vice.com/blog/tim-burgess-white-video-exclusive",
+  "http://thequietus.com/articles/09799-listen-sam-willis-winterval"
   "https://www.google.co.uk/search?q=dan+tombs+visuals&tbm=isch&tbo=u&source=univ&sa=X&ei=YfduUbnjMKLy4QSe5YCADg&ved=0CGMQsAQ&biw=1017&bih=856"
 ];
 // This array contains the image list. 
@@ -20,5 +35,14 @@ window.images = [
   "brutality.gif",
   "muscle_beach.gif",
   "hyetal.gif",
+  "feedback_dt_01.gif",
+  "feedback_dt_02.gif",
+  "feedback_dt_03.gif",
+  "feedback_dt_04.gif",
+  "feedback_dt_05.gif",
+  "feedback_dt_06.gif",
+  "feedback_dt_07.gif",
+  "addidas_underground_ff.gif",
+  "cube_gif.gif",
   "ica.gif"
 ];
