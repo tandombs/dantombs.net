c0d568585102616c03bad02d20d67a783f4fd5a4
Alex Sanders <a@snd.rs>
2016-03-14 22:48:50 +0000

    autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const postcss      = require('gulp-postcss');
+const sourcemaps   = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
 
 gulp.task('default', () => {
 });
 
 gulp.task('sass', () => gulp.src("src/scss/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(postcss([
+        autoprefixer()
+    ]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest("build/css"))
     .pipe(browserSync.stream())
 );
